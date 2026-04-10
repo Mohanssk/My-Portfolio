5e7c73c635598b18ed586cce507d4f9e52d2a65d
mohanssk <mohankaruparthi7@gmail.com>
2026-04-11 03:34:53 +0530

    Modified Tree Struture and styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -45,7 +45,7 @@ function About() {
             showIcon={false}
             showUserInfo={false}
             enableTilt={true}
-            enableMobileTilt={false}
+            enableMobileTilt={true}
             onContactClick={() => console.log('Contact clicked')}
             behindGlowColor="rgba(125, 190, 255, 0.67)"
             behindGlowEnabled={false}
